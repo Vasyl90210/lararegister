3d82a11f36c151b278c348c4e832b8dbcf23e965
Vasyl90210 <feelin20@gmail.com>
2024-04-17 09:28:32 +0300

    Исправление ошибок

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,18 @@
         @yield('content')
     </div>
     <!-- Подключение скриптов Bootstrap -->
+    <script>
+        // Добавляем состояние истории при загрузке страницы
+        window.history.pushState({ page: 1 }, "", "");
+        // Обработчик события нажатия кнопки "Назад"
+        window.addEventListener("popstate", function(event) {
+            // Проверяем, авторизован ли пользователь
+            if (!{{ auth()->check() ? 'true' : 'false' }}) {
+                // Если пользователь не авторизован, делаем редирект на страницу входа
+                window.location.href = '{{ route('login') }}';
+            }
+        });
+    </script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
